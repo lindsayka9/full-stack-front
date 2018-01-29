628848d0ddc021ee27c5aff19682cf32c9505868
lindsayka9 <kat.lindsay.92@gmail.com>
2018-01-29 16:16:14 -0500

    Owners templates
    Add single and multiple owner templates
    Duplicate pets events for owners

--- assets/scripts/api.js
+++ assets/scripts/api.js
@@ -96,6 +96,61 @@ const updatePet = function (data) {
   })
 }
 
+const createOwner = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/owners',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const showOwners = function () {
+  return $.ajax({
+    url: config.apiOrigin + '/owners',
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const showOwner = function (data) {
+  // console.log(data)
+  return $.ajax({
+    url: config.apiOrigin + '/owners/' + data.owner.id,
+    method: 'GET',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    data
+  })
+}
+
+const deleteOwner = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/owners/' + data.owner.id,
+    method: 'DELETE',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    }
+  })
+}
+
+const updateOwner = function (data) {
+  return $.ajax({
+    url: config.apiOrigin + '/owners/' + data.owner.id,
+    method: 'PATCH',
+    headers: {
+      Authorization: 'Token token=' + store.user.token
+    },
+    // contentType: 'application/json',
+    data
+  })
+}
+
 module.exports = {
   signUp,
   signIn,
@@ -105,5 +160,10 @@ module.exports = {
   showPets,
   deletePet,
   updatePet,
-  showPet
+  showPet,
+  createOwner,
+  showOwners,
+  deleteOwner,
+  updateOwner,
+  showOwner
 }
